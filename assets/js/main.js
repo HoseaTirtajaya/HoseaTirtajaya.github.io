61a65689bfcf7f0674eb89334ca1503d20eda271
HoseaTirtajaya <hoseatirtajaya@gmail.com>
2020-05-24 20:41:35 +0700

    Initial Commit :full_moon:

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,20 @@
 const hButton = document.getElementById("HomeButton");
 const aButton = document.getElementById("AboutButton");
 const pButton = document.getElementById("PortfolioButton");
+const sect1 = document.querySelector(".blank");
+const sect2 = document.querySelector(".blank2");
 
 hButton.addEventListener("click", function () {
   window.scrollTo(0, 0);
+  alert("Web is under development. Thank you for your understanding");
+});
+
+aButton.addEventListener("click", function () {
+  sect1.scrollIntoView();
+  alert("Web is under development. Thank you for your understanding");
+});
+
+pButton.addEventListener("click", function () {
+  sect2.scrollIntoView();
+  alert("Web is under development. Thank you for your understanding");
 });
