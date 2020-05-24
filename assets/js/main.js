390d34a3d905389233229b1a382840ace140538a
HoseaTirtajaya <hoseatirtajaya@gmail.com>
2020-05-24 18:47:58 +0700

    Initial Commit :full_moon:

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+const hButton = document.getElementById("HomeButton");
+const aButton = document.getElementById("AboutButton");
+const pButton = document.getElementById("PortfolioButton");
+
+hButton.addEventListener("click", function () {
+  window.scrollTo(0, 0);
+});
